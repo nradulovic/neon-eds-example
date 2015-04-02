175e691175684d7317f56aa9622faab3efe5f023
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2015-04-02 20:30:35 +0200

    Added NUCLEO-F411RE board

--- port/stm32f4xx-hal/bsp.c
+++ port/stm32f4xx-hal/bsp.c
@@ -11,13 +11,19 @@
 
 /*=========================================================  LOCAL MACRO's  ==*/
 
-#define STM_BOARD_STM32F429I_DISCO      1
+#define STM_BOARD_STM32F429I_DISCO      0
+#define STM_BOARD_STM32F411E_DISCO      1
 
 #if (STM_BOARD_STM32F429I_DISCO == 1)
 #define LED_PIN                                 GPIO_PIN_13
 #define LED_GPIO_PORT                           GPIOG
 #define LED_GPIO_CLK_ENABLE()                   __GPIOG_CLK_ENABLE()
 #define LED_GPIO_CLK_DISABLE()                  __GPIOG_CLK_DISABLE()
+#elif (STM_BOARD_STM32F411E_DISCO == 1)
+#define LED_PIN                                 GPIO_PIN_5
+#define LED_GPIO_PORT                           GPIOA
+#define LED_GPIO_CLK_ENABLE()                   __GPIOA_CLK_ENABLE()
+#define LED_GPIO_CLK_DISABLE()                  __GPIOA_CLK_DISABLE()
 #endif
 
 /*======================================================  LOCAL DATA TYPES  ==*/
@@ -83,6 +89,63 @@ static void SystemClock_Config(void)
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
     HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+}
+#elif (STM_BOARD_STM32F411E_DISCO == 1)
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSI)
+  *            SYSCLK(Hz)                     = 100000000
+  *            HCLK(Hz)                       = 100000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSI Frequency(Hz)              = 16000000
+  *            PLL_M                          = 16
+  *            PLL_N                          = 400
+  *            PLL_P                          = 4
+  *            PLL_Q                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale2 mode
+  *            Flash Latency(WS)              = 3
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* The voltage scaling allows optimizing the power consumption when the device is
+     clocked below the maximum system frequency, to update the voltage scaling value
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /* Enable HSI Oscillator and activate PLL with HSI as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 0x10;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 400;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
 }
 #endif
 
